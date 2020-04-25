e7d948112f35fa88db3f1ab786d5ae20129f2801
Tom <revilojapan@gmail.com>
2020-04-25 19:02:36 +0900

    hily shit epic query boyo

--- queries/1.sql
+++ queries/1.sql
@@ -1,15 +1,25 @@
 
+select 
+*
+-- count(*) 
+from
+
+    (select group_concat(jisho), hiragana, count(hiragana) numberOfWords, 
+    group_concat(distinct downstep) downStepPatterns, count (distinct downstep) c from
+
+        (select jisho, hiragana, downstep from Word w join Hiragana h on w.id = h.wordId
+                                                    join Reading r on w.id = r.wordId
+        group by jisho
+    )
 
 
-select hiragana, count(hiragana) numberOfWords, group_concat(distinct downstep) downStepPatterns from
 
-    (select jisho, hiragana, downstep from Word w join Hiragana h on w.id = h.wordId
-                                                join Reading r on w.id = r.wordId
-    group by jisho
-   )
+    group by hiragana
+    -- having
+    -- c > 2
+    order by length(downStepPatterns) desc)
 
-group by hiragana
-order by length(downStepPatterns) desc
+
 
 
 
