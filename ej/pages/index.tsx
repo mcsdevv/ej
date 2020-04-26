34bfff8f4b5287c20313de3c98b36228ca28b95f
Tom <revilojapan@gmail.com>
2020-04-26 15:49:27 +0900

    added next

---
+++ ej/pages/index.tsx
@@ -0,0 +1,10 @@
+import ReactAudioPlayer from 'react-audio-player'
+
+export default function Home() {
+	return (
+		<div className='container'>
+			hfllo
+			<ReactAudioPlayer src='my_audio_file.ogg' autoPlay controls />
+		</div>
+	)
+}
