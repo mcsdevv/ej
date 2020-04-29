f0b1f2b34534a2bf782b55c13b89c66d99df6ad0
Tom <revilojapan@gmail.com>
2020-04-29 19:22:39 +0900

    looks decent

--- components/accentWord/container.tsx
+++ components/accentWord/container.tsx
@@ -1,4 +1,4 @@
-import { cloneDeep } from 'lodash'
+import { cloneDeep, chunk, range } from 'lodash'
 import { DndProvider } from 'react-dnd'
 import Backend from 'react-dnd-html5-backend'
 import { useState, useEffect } from 'react'
@@ -8,8 +8,8 @@ type Props = {
 	downStep: number
 }
 export default function({ hiragana, downStep }: Props) {
-	const width = 800
-	const height = 300
+	const width = 500
+	const height = 140
 
 	const [ array, setArray ] = useState(Array(hiragana.length).fill(false))
 
@@ -25,6 +25,7 @@ export default function({ hiragana, downStep }: Props) {
 		.map((x, i) => (
 			<Col
 				key={i}
+				letter={x}
 				index={i}
 				conHeight={height}
 				conWidth={width}
@@ -34,25 +35,76 @@ export default function({ hiragana, downStep }: Props) {
 			/>
 		))
 
+	const lines = range(0, hiragana.length - 1).map((i) => {
+		return (
+			<Pair
+				key={i}
+				index={i}
+				conHeight={height}
+				conWidth={width}
+				length={hiragana.length}
+				high1={array[i]}
+				high2={array[i + 1]}
+			/>
+		)
+	})
+
 	return (
 		<svg width={width} height={height}>
+			{lines}
 			{columns}
 		</svg>
 	)
 }
 
-const Col = ({ index, conWidth, conHeight, length, high, onClick }) => {
-	const radius = Math.min(conWidth / (length * 4), conHeight / 4)
+const caluclatePosition = (conWidth, conHeight, length, high) => {
+	const sWidth = 3
+	const radius = Math.min(conWidth / (length * 4), conHeight / 4) - 2 * sWidth
 	const wInterval = conWidth / length + radius / 2
 	const hInterval = high ? 2 * radius : conHeight - 2 * radius
 
+	return { radius, wInterval, hInterval, sWidth }
+}
+
+const Pair = ({ index, conWidth, conHeight, length, high1, high2 }) => {
+	let { radius, wInterval, hInterval } = caluclatePosition(conWidth, conHeight, length, high1)
+	const hInterval1 = hInterval
+	const hInterval2 = caluclatePosition(conWidth, conHeight, length, high2).hInterval
+
 	return (
-		<a>
-			<g onClick={onClick} style={{ pointerEvents: 'all' }}>
-				<circle cy={hInterval} cx={radius + index * wInterval} r={radius} fill='red' />
-				{/* The rectangle is invisible and is only here to make the g tag fill the entire column */}
-				<rect x={conWidth / length * index} width={conWidth / length} height={conHeight} fill={'none'} />
+		<line
+			x1={radius + index * wInterval}
+			x2={radius + (index + 1) * wInterval}
+			y1={hInterval1}
+			y2={hInterval2}
+			style={{ stroke: 'green', strokeWidth: '10px' }}
+		/>
+	)
+}
+
+const Col = ({ letter, index, conWidth, conHeight, length, high, onClick }) => {
+	let { radius, wInterval, hInterval, sWidth } = caluclatePosition(conWidth, conHeight, length, high)
+	return (
+		<g onClick={onClick} style={{ pointerEvents: 'all' }}>
+			<g>
+				<circle
+					cy={hInterval}
+					cx={radius + index * wInterval}
+					r={radius}
+					fill='red'
+					style={{ strokeWidth: `${sWidth}px`, stroke: 'blue' }}
+				/>
+				<text
+					width={radius * 2}
+					y={hInterval + radius / 2}
+					x={+index * wInterval + radius / 4}
+					style={{ fontSize: `${radius * 1.5}` }}
+				>
+					{letter}
+				</text>
 			</g>
-		</a>
+			{/* The rectangle is invisible and is only here to make the g tag fill the entire column */}
+			<rect x={conWidth / length * index} width={conWidth / length} height={conHeight} fill={'none'} />
+		</g>
 	)
 }
